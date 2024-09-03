d722f16a5f27b22d4024c4ab6bbd4e1759c3ea5f
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2024-09-03 15:42:44 +0900

    ApplicativeError

---
+++ src/main/scala/TypeClass/ApplicativeErrorSample.scala
@@ -0,0 +1,30 @@
+package TypeClass
+
+import cats.ApplicativeError
+import cats.instances.either.*
+
+object ApplicativeErrorSample {
+
+  // LeftをStringで表現するEither
+  // ここをOptionとかTryに切り替えても、利用者側からは同じようにエラーを扱える?ことがメリットだろうか？
+  // OptionとEitherとTryのApplicativeErrorをimplicit valで定義しておけば広い範囲でエラーの扱い方を統一できることもメリット？
+  type ErrorOr[A] = Either[String, A]
+
+  // 左はエラー型、右はエラーが起こるかも知れないなにか
+  val applicativeError = ApplicativeError[ErrorOr, String]
+
+  // raiseErrorでエラーの生成する
+  val error: ErrorOr[Int] = applicativeError.raiseError("Something went wrong")
+
+  // エラー型を処理するときにhandleErrorWithをつかう(E => Aの関数を取るhandleErrorというのもあるよ)
+  val handled: ErrorOr[Int] = applicativeError.handleErrorWith(error) {
+    case "Something went wrong" => Right(0) // サンプルなのでdefault値を返すってことで
+  }
+
+  // 計算を実行
+  // ErrorOrがEitherのエイリアスとしてFになっているが、ここでは成功したときに返す型もEitherであることに注意
+  // handledはただの変数で失敗しないのでEitherになって返ってくるだけ
+  // attempt(fa: F[A]): F[Either[E, A]]
+  val attempt: ErrorOr[Either[String, Int]] = applicativeError.attempt(handled)
+
+}
