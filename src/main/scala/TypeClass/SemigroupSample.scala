07c95ca2a01014506e38a57c9cffeb760892d52a
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2025-02-08 08:44:44 +0900

    Samigroup

---
+++ src/main/scala/TypeClass/SemigroupSample.scala
@@ -0,0 +1,25 @@
+package TypeClass
+
+import cats.kernel
+
+// セミグループは"足せる"ってことさ
+trait Semigroup[A] {
+  def combine(x: A, y: A): A
+  // combine(x, combine(y, z)) = combine(combine(x, y), z) のように、計算の順序は関係ないので並列にしたりできる
+}
+
+object SemigroupSample {
+  // Intにおいて"足す"がなになのかを定義する
+  implicit val intAdditionSemigroup: Semigroup[Int] = _ + _
+
+  val x = 1
+  val y = 2
+  intAdditionSemigroup.combine(x, y)
+  // Int = 3
+
+
+  // 専用のシンタックスがあるぞ
+  import cats.syntax.all._
+  1 |+| 2
+  // res4: Int = 3
+}
