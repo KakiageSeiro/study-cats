acab96c9679b5d18349b25c7a3e6611ce290861f
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2025-02-08 22:16:45 +0900

    FreeApplication途中

--- src/main/scala/DataTypes/FreeApplicativeSample.scala
+++ src/main/scala/DataTypes/FreeApplicativeSample.scala
@@ -75,3 +75,59 @@ val parCompiler = new FunctionK[ValidationOp, ParValidator] {
 
 // ここでfoldMapする時に、Futureだから並列に処理できる
 val parValidator: ParValidator[Boolean] = prog.foldMap[ParValidator](parCompiler)
+
+
+
+
+// ---------------------------------どのバリデーションがされたかをログにだすケースの使い方？
+import cats.data.Const
+
+// Constは第２パラメータを無視する型。ここでは第１パラメータのList[String]はログを累積する用途につかう
+// 単なるList[String]ではないのは、ConstがCatsの抽象化(FunctionKとかかな)の仕組みを使うことでロジックの全体でCatsの仕組みを使ってることになり、見やすくなったりするから？
+// あとfoldMapするときに累積されるが、累積ロジックはListそのものにはないが、Constで包むことで累積ロジックを書かなくてもよくなってる？
+type Log[A] = Const[List[String], A]
+
+val logCompiler = new FunctionK[ValidationOp, Log] {
+  def apply[A](fa: ValidationOp[A]): Log[A] = fa match {
+    case Size(size) => Const(List(s"size >= $size"))
+    case HasNumber => Const(List("has number"))
+  }
+}
+
+object FreeApplicativeSample_Log {
+  def logValidation[A](validation: Validation[A]): List[String] =
+    validation.foldMap[Log](logCompiler).getConst
+
+  def logValidation_prog(): Seq[String] =
+    logValidation(prog)
+    // res2: List[String] = List("size >= 5", "has number")
+
+  // *> は「前の結果を捨てて次に進む」というシーケンス演算子
+  def logValidation_size_hasNumber_size(): Seq[String] =
+    logValidation(size(5) *> hasNumber *> size(10))
+    // res3: List[String] = List("size >= 5", "has number", "size >= 10")
+
+  // || で論理和ととってるが、意味はなくて呼び出されていることだけ確認する
+  def logValidation_map2(): Seq[String] =
+    logValidation((hasNumber, size(3)).mapN(_ || _))
+    // res4: List[String] = List("has number", "size >= 3")
+}
+
+
+// ----------------------------------------並列のログの解釈器を合成できる
+import cats.data.Tuple2K
+
+object FreeApplicativeSample_Par_Log {
+  type ValidateAndLog[A] = Tuple2K[ParValidator, Log, A]
+  val prodCompiler: FunctionK[ValidationOp, ValidateAndLog] = parCompiler and logCompiler
+  val prodValidation: ValidateAndLog[Boolean] = prog.foldMap[ValidateAndLog](prodCompiler)
+}
+
+
+
+
+
+
+
+
+
--- src/test/scala/DataTypes/FreeApplicativeSampleTest.scala
+++ src/test/scala/DataTypes/FreeApplicativeSampleTest.scala
@@ -3,6 +3,11 @@ package DataTypes
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import cats.data.Const
+import cats.syntax.all.*
+
+import scala.concurrent.Future
+
 
 class FreeApplicativeSampleTest extends AnyFlatSpec with ScalaFutures with Matchers {
 
@@ -11,5 +16,56 @@ class FreeApplicativeSampleTest extends AnyFlatSpec with ScalaFutures with Match
     assert(FreeApplicativeSample.validator("12345"))
   }
 
+  "Log" should "が動く" in {
+    assert(FreeApplicativeSample_Log.logValidation_prog() == List("size >= 5", "has number"))
+    assert(FreeApplicativeSample_Log.logValidation_size_hasNumber_size() == List("size >= 5", "has number", "size >= 10"))
+    assert(FreeApplicativeSample_Log.logValidation_map2() == List("has number", "size >= 3"))
+  }
 
+  // "並列とLogセットの解釈器" should "が動く" in {
+  //   // 入力 "1234" は長さが 4 なので Size(5) には引っかかり、かつ "1234" は数字を含むので HasNumber は true
+  //   // prog は両方の結果を && で結合しているため、結果は false となるはずです
+  //   val (resultInvalid, logsInvalid) = runValidateAndLog(FreeApplicativeSample_Par_Log.prodValidation)("1234")
+  //   resultInvalid shouldBe false
+  //   // ログは prog 内での操作順に従い、"size >= 5" と "has number" が蓄積されるはず
+  //   logsInvalid shouldEqual List("size >= 5", "has number")
+  //
+  //   // 入力 "12345" は長さ 5 なので Size(5) をパスし、かつ数字を含むので HasNumber も true
+  //   // したがって結果は true になるはずです
+  //   val (resultValid, logsValid) = runValidateAndLog(FreeApplicativeSample_Par_Log.prodValidation)("12345")
+  //   resultValid shouldBe true
+  //   logsValid shouldEqual List("size >= 5", "has number")
+  // }
+
+
+  "並列とログを両方やる" should "バリデーションとログ集めを両方やれている" in {
+    // ValidateAndLog[A]はTuple2K[ParValidator, Log, A] なので、まずはそれを実行するヘルパーを定義
+    def runValidateAndLog[A](valAndLog: FreeApplicativeSample_Par_Log.ValidateAndLog[A])(input: String): Future[(A, List[String])] = {
+      implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+
+      val validator: ParValidator[A] = valAndLog.first
+      val log: Log[A] = valAndLog.second
+
+      // Kleisli[Future, String, A] は String => Future[A] のラッパー
+      // validatorはKleisli[Future, String, A]なので、run(input)でFuture[A]を取れる
+      val resultF: Future[A] = validator.run(input)
+
+      // Log[A] = Const[List[String], A]
+      // getConstで内部のList[String]を取れる
+      val logs: List[String] = log.getConst
+      resultF.map(result => (result, logs))
+    }
+
+    val prodVal = FreeApplicativeSample_Par_Log.prodValidation
+
+    // "1234"では、Size(5)は false(文字列長 4 < 5)なので、結果はfalseになる
+    val (result1, logs1) = runValidateAndLog(prodVal)("1234").futureValue
+    assert(!result1)
+    assert(logs1 == List("size >= 5", "has number"))
+
+    // "12345"では、検証条件を両方ともパスするので結果はtrue
+    val (result2, logs2) = runValidateAndLog(prodVal)("12345").futureValue
+    assert(result2)
+    assert(logs2 == List("size >= 5", "has number"))
+  }
 }
